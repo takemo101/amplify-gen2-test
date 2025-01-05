19cadc3097bd4b271eb1ca3d9dbb177613ff7e71
takemo101 <takemo101@gmail.com>
2025-01-06 08:28:46 +0900

    [refactor] amplify/data/resource.ts: amplify/auth/resource.tsのメール検証の件名を更新しました

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import './../app/app.css';
 import outputs from '@/amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
@@ -16,14 +17,18 @@ export default function App() {
   const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
 
   function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
+    const sub = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    return () => sub.unsubscribe();
   }
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    listTodos();
+    const unsub = listTodos();
+
+    return () => unsub();
   }, []);
 
   function createTodo() {
@@ -32,24 +37,33 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
     <main>
       <h1>My todos</h1>
-      <button type="button" onClick={createTodo}>
-        + new
-      </button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <>
+            <button type="button" onClick={createTodo}>
+              + new
+            </button>
+            <ul>
+              {todos.map((todo) => (
+                <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                  {todo.content}
+                </li>
+              ))}
+            </ul>
+            <button type="button" onClick={signOut}>
+              Sign out
+            </button>
+            <div>{user?.username}</div>
+          </>
+        )}
+      </Authenticator>
     </main>
   );
 }
