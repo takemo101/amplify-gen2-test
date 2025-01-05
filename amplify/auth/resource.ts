6e821e83e6a35294c09ab0cf7c5245d61c51672e
takemo101 <takemo101@gmail.com>
2025-01-06 08:20:24 +0900

    [refactor] Update email verification subject in amplify/auth/resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,8 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: 'Your verification code',
+    },
   },
 });
